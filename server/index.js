9308afd56b9840f2194aef91ac25d5d57641f712
Suraj7789 <77788438+Suraj7789@users.noreply.github.com>
2025-01-26 19:47:48 +0530

    added new comment

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
 app.get("/", (req, res) => res.json({ msg: "Hello world" }));
+// yo yo honey singh
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
